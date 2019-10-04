80b9e30d684ddbd3fcf2fafec6363f0124bac0d8
Arthur <aptyp.potatoch@gmail.com>
2019-10-05 02:57:44 +0300

    state output messages added

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "ret_codes.h"
+#include "process_tools.h"
+#include "io_tools.h"
 
+
+
+void show_message(int rc);
 
 int main()
 {
@@ -12,3 +17,24 @@ int main()
 	
 	return rc;
 }
+
+void show_message(int rc)
+{
+	switch (rc)
+	{
+		case NO_ARITHM_SEQ:
+			printf("В данном массиве слишком мало элементов
+					для арифметической последовательности.\n");
+			break;
+		case INPUT_ERR:
+			printf("Неверный ввод. Ожидается неотриательная
+					длина и целочисленные элеенты массива.\n");
+			break;
+		case ALLOC_ERR:
+			printf("Ошибка выделения памяти.\n");
+			break;
+		case HAPPY_END:
+			printf("Программа успешно завершилась.\n");
+	}
+}
+
