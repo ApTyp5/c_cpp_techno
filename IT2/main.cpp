a3f1f20eecc5a91eb87de7b2cedfdba8b3d17dff
Arthur <aptyp.potatoch@gmail.com>
2019-10-20 23:49:08 +0300

    Compare test done

--- IT2/main.cpp
+++ IT2/main.cpp
@@ -7,51 +7,62 @@
 #include <gtest/gtest.h>
 #include <random>
 #include <cstddef>
+#include <ctime>
 
 Vector_lt random_Vector_lt(Vector_lt min, Vector_lt max) {
-
-    return (static_cast<Vector_lt>(rand()) / RAND_MAX) * (max - min) + min;
+    srand(time(NULL));
+    return (static_cast<Vector_lt>(rand()) * (max - min) / RAND_MAX)  + min;
 }
 
 littleVector_t *create_test_vector_arr(size_t size){
-    auto *res = new littleVector_t[size];
+    auto res = (littleVector_t *)malloc(sizeof(littleVector_t) * size);
+    // Проверяю успешность malloc таким образом, так как функция тепличная,
+    // используется только в тесках
+    assert(res);
     for (size_t i = 0; i < size; i++)
         for (size_t j = 0; j < VecSize; j++)
-            res[i][j] = random_Vector_lt(-MAXFLOAT, MAXFLOAT);
+            res[i][j] = random_Vector_lt(-1000, 1000);
 
     return res;
 }
 
 littleVector_t *copy_test_vector_arr(littleVector_t *arr1, size_t size){
-    auto *res = new littleVector_t[size];
+    auto res = (littleVector_t *)malloc(sizeof(littleVector_t) * size);
+    assert(res);
     for (size_t i = 0; i < size; i++)
         for (size_t j = 0; j < VecSize; j++)
             res[i][j] = arr1[i][j];
 
     return res;
 }
-/*
-TEST(compareTest, size5){
-    size_t size = 5;
-    littleVector_t ans1;
-    littleVector_t ans2;
-    littleVector_t *arr1 = create_test_vector_arr(size);
-    littleVector_t *arr2 = copy_test_vector_arr(arr1, size);
-    simpleAvgVector(ans1, arr1, size);
-    parallelAvgVector(ans2, arr2, size);
-    for (size_t i = 0; i < VecSize; i++){
-        ASSERT_EQ(ans1[i], ans2[i]);
+
+TEST(compareTest, sizes_10_10000){
+    for (size_t size = 10; size < 10001; size *= 10){
+        littleVector_t ans1, ans2;
+        littleVector_t *arr1 = create_test_vector_arr(size);
+        littleVector_t *arr2 = copy_test_vector_arr(arr1, size);
+
+        simpleAvgVector(ans1, arr1, size);
+        parallelAvgVector(ans2, arr2, size);
+        for (size_t i = 0; i<VecSize; i++){
+            ASSERT_FLOAT_EQ(ans1[i], ans2[i]);
+        }
+
+        free(arr1);
+        free(arr2);
     }
+
 }
-*/
+
+
 
 
 int main(int argc, char *argv[]){
-    /*
+   //*
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    */
-
+  //  */
+/*
     size_t size = 5;
     littleVector_t ans1;
     littleVector_t ans2;
@@ -59,6 +70,7 @@ int main(int argc, char *argv[]){
     littleVector_t *arr2 = copy_test_vector_arr(arr1, size);
     simpleAvgVector(ans1, arr1, size);
     parallelAvgVector(ans2, arr2, size);
+    */
 }
 
 
--- IT2/parallelAvg.cpp
+++ IT2/parallelAvg.cpp
@@ -19,7 +19,7 @@ void *summer(void *sumArgs) {
     size_t size             = summerArgs_ptr->size;
 
     bigVector_t sum = {0};
-    for (size_t i = 0; i < summerArgs_ptr->size; i++)
+    for (size_t i = 0; i < size; i++)
         for (size_t j = 0; j < VecSize; j++)
             sum[j] += vectors[i][j];
 
@@ -31,13 +31,18 @@ void *summer(void *sumArgs) {
 }
 
 size_t find_core_num(){
-    size_t core_num = system("cat /proc/cpuinfo|grep processor|wc -l");
+    /*
+    size_t core_num = system("cat /proc/cpuinfo | grep processor | wc -l");
+    */
+    size_t core_num = 4;
     assert(core_num < MAX_CORES);
+
     return core_num;
 }
 
 inline void start_all_threads(summerArgs args[], pthread_t threads[],
-                              littleVector_t vectors[], size_t core_num, size_t el_per_core, size_t size){
+                              littleVector_t vectors[], size_t core_num,
+                              size_t el_per_core, size_t size){
 
     for (size_t i = 0; i < core_num - 1; i++){
         args[i] = {vectors + i * el_per_core, el_per_core};
@@ -47,7 +52,7 @@ inline void start_all_threads(summerArgs args[], pthread_t threads[],
     size_t last_i = core_num - 1;
     size_t elements_processed = el_per_core * last_i;
     args[last_i] = {vectors + elements_processed,size - elements_processed};
-    pthread_create(threads + last_i, nullptr, summer, args);
+    pthread_create(threads + last_i, nullptr, summer, args + last_i);
 }
 
 inline void wait_all_threads(pthread_t threads[], size_t core_num){
@@ -60,10 +65,10 @@ inline void count_answer(littleVector_t avg_vector, summerArgs args[],
                         size_t core_num, size_t size){
     bigVector_t sum = {0};
     for (size_t i = 0; i < core_num; i++)
-        for (size_t j = 0; j < core_num; j++)
+        for (size_t j = 0; j < VecSize; j++)
             sum[j] += args[i].sum[j];
 
-    for (size_t j = 0; j < core_num; j++){
+    for (size_t j = 0; j < VecSize; j++){
         sum[j] /= size;
         avg_vector[j] = (Vector_lt) sum[j];
     }
@@ -71,9 +76,11 @@ inline void count_answer(littleVector_t avg_vector, summerArgs args[],
 
 int parallelAvgVector(littleVector_t avg_vector, littleVector_t vectors[], size_t size)
 {
+    /*
     if (unlikely((avg_vector == nullptr)
                      ||(vectors == nullptr)))   return EXIT_NULL_REC;
-
+*/
+    if (avg_vector == nullptr || vectors == nullptr) return EXIT_NULL_REC;
     if (unlikely(size == 0))  return EXIT_ZERO_SIZ;
 
     // Считаем клоличество логических ядер процессра
@@ -86,7 +93,7 @@ int parallelAvgVector(littleVector_t avg_vector, littleVector_t vectors[], size_
     summerArgs args[core_num];
     size_t el_per_core = size / core_num;
 
-    start_all_threads(args, threads, vectors,core_num,  el_per_core, size);
+    start_all_threads(args, threads, vectors,core_num, el_per_core, size);
     wait_all_threads(threads, core_num);
     count_answer(avg_vector, args, core_num, size);
     return EXIT_SUCCESS;
